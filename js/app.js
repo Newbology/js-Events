dd8e79307ab1a7d726015837fa87335da2788535
kevin tauzer <kevintauzer97@gmail.com>
2018-10-21 13:23:19 -1000

    completed #4, skipped 3 for now

--- js/app.js
+++ js/app.js
@@ -58,7 +58,21 @@ function lessContent(){
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
+var menuItems = ["whopper", "Chalupa", "Ultimate cheese burger"]
 
+showMenu.addEventListener('click', valuMenu)
+
+menu.innerHTML=menuItems;
+
+menu.style.display='none';
+
+function valuMenu(){
+    if(menu.style.display==='block'){
+        menu.style.display = "none"
+    }else{
+        menu.style.display='block'
+    }
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
